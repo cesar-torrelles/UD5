f6cede9d89e8ad83fcfe65bdfddbb634d861bd1f
cesar-torrelles <raseksk8@gmail.com>
2021-02-05 00:59:29 +0100

    t5.7

---
+++ T5.7/src/ejercicio7App.java
@@ -0,0 +1,24 @@
+
+public class ejercicio7App {
+	
+	public static void main(String[] args) {
+
+		//declaramos la variable "A" con el valor inicial 1
+		int A = 1;
+		
+		//realizamos un loop while para que imprima todos los valores de "A" al 100  incluido
+		// y que vaya imprimiendo en consola en una linea nueva cada valor hasta llegar a 100 
+		
+		while(A<=100) {
+			
+			System.out.println(A);
+			
+		A++;
+			
+		
+		}
+	}
+		
+}
+
+
