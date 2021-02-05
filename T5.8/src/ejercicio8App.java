2d1cf0efc7a93d85095c576cd889af16d0e0629d
cesar-torrelles <raseksk8@gmail.com>
2021-02-05 01:01:03 +0100

    t5.8

---
+++ T5.8/src/ejercicio8App.java
@@ -0,0 +1,14 @@
+
+public class ejercicio8App {
+
+	public static void main(String[] args){
+		
+		
+		//realizamos un loop for para que imprima todos los valores de "A" al 100  incluido
+		// y que vaya imprimiendo en consola en una linea nueva cada valor hasta llegar a 100 
+		for(int i=1;i<=100;i++) {
+			System.out.println(i);
+		}
+	}
+
+}
